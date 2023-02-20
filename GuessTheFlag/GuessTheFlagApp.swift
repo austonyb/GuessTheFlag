122049a9da33400f6b379ac3e2c0aa5055f95c91
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-02-20 11:46:48 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Auston Youngblood on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
